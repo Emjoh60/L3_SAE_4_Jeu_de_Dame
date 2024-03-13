dd4b50783bdc31b0882f210281181696b5174d11
Johann Thomas <125392656+Emjoh60@users.noreply.github.com>
2024-03-13 17:48:16 +0100

    Add files via upload

--- window.py
+++ window.py
@@ -1,130 +1,91 @@
 import pygame
 import webbrowser
+import constants
 
-# Fonction qui permet de savoir si un elt est cliquer
-def checkClique(pos):
-    global running
-    if (startButton.rect.collidepoint(pos)):
-        print("start")
-    elif (exitButton.rect.collidepoint(pos)):
-        running=False
-    elif (githuButton.rect.collidepoint(pos)):
-        webbrowser.open("https://github.com/Emjoh60/L3_SAE_4_Jeu_de_Dame")
+WINDOW_LONG=850
+WINDOW_LARG=480
 
-def checkMotion(pos):
-    if (startButton.rect.collidepoint(pos) and not startButton.motion):
-        startButton.motion = True
-    elif (not startButton.rect.collidepoint(pos) and startButton.motion):
-        startButton.motion = False
-    if (exitButton.rect.collidepoint(pos) and not exitButton.motion):
-        exitButton.motion = True
-    elif (not exitButton.rect.collidepoint(pos) and exitButton.motion):
-        exitButton.motion = False
-    if (githuButton.rect.collidepoint(pos) and not githuButton.motion):
-        githuButton.motion = True
-    elif (not githuButton.rect.collidepoint(pos) and githuButton.motion):
-        githuButton.motion = False
-        
-
-# Initialisation de pygame
-pygame.init()
-
-# Création d'une fenêtre
-pygame.display.set_caption("Jeu de Dame")
-screen = pygame.display.set_mode((850, 478))
-
-# Charger une image
-background = pygame.image.load('ressources/bkgrd.jpg')
-background.convert()
-
-# Télécharger les boutons d'images
-startImg = pygame.image.load('ressources/start.png').convert_alpha()
-exitImg = pygame.image.load('ressources/exit.png').convert_alpha()
-logoGitHub = pygame.image.load('ressources/github.png').convert_alpha()
-npIcone = pygame.image.load('ressources/np.png').convert_alpha()
-startImgClicked = pygame.image.load('ressources/startClicked.png').convert_alpha()
-exitImgClicked = pygame.image.load('ressources/exitClicked.png').convert_alpha()
-logoGitHubClicked = pygame.image.load('ressources/githubClicked.png').convert_alpha()
-
-class Button1:
+class Button:
     def __init__(self, x, y, image, imageCover):
         self.image = image
         self.imageCover = imageCover
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.topleft = (x, y)
         self.motion = False
         
     def redraw(self):
+        global screen
         if not self.motion :
             screen.blit(self.image, (self.rect.x, self.rect.y))
         else :
             screen.blit(self.imageCover, (self.rect.x, self.rect.y))
-        
-class Button2:
-    def __init__(self, x, y, image, imageCover):
-        self.image = image
-        self.imageCover = imageCover
-        self.rect = self.image.get_rect()
-        self.rect.topright = (x, y)
-        self.motion = False
-        
-    def redraw(self):
-        if not self.motion :
-            screen.blit(self.image, (self.rect.x, self.rect.y))
-        else :
-            screen.blit(self.imageCover, (self.rect.x, self.rect.y))
-        
-        
-class Button3:
-    def __init__(self, x, y, image, imageCover):
-        self.image = image
-        self.imageCover = imageCover
-        self.rect = self.image.get_rect()
-        self.rect.bottomleft = (x, y)
-        self.motion = False
-        
-    def redraw(self):
-        if not self.motion :
-            screen.blit(self.image, (self.rect.x, self.rect.y))
-        else :
-            screen.blit(self.imageCover, (self.rect.x, self.rect.y))
-        
-class Button4:
-    def __init__(self, x, y, image):
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.bottomright = (x, y)
-        
-    def draw(self):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-        
-# Création de boutton
-startButton = Button1(425, 239, startImg, startImgClicked)
-exitButton = Button2(825, 10, exitImg, exitImgClicked)
-githuButton = Button3(25, 468, logoGitHub, logoGitHubClicked)
-npButton = Button4(825, 468, npIcone)
 
+def checkMotion(pos,listBouton):
+    for bouton in listBouton:
+        if (bouton.rect.collidepoint(pos) and not bouton.motion):
+            bouton.motion = True
+        elif (not bouton.rect.collidepoint(pos) and bouton.motion):
+            bouton.motion = False
+        
+def start():
+    # Initialisation de pygame
+    pygame.init()
 
+    # Création d'une fenêtre
+    pygame.display.set_caption("Jeu de Dame")
+    screen = pygame.display.set_mode((WINDOW_LONG, WINDOW_LARG))
 
-# Affichage de la fenêtre
-running = True
-while running:
-    screen.blit(background, (0, 0))
-    startButton.redraw()
-    exitButton.redraw()
-    githuButton.redraw()
-    npButton.draw()
-    pygame.display.flip()
-        
-    # Boucle pour défénir une action faite par l'utilisateur
-    for event in pygame.event.get(): 
-        # Si personne fait une action on demande de quitter le jeu
-        if event.type == pygame.QUIT: 
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            checkClique(event.pos)
-        if event.type == pygame.MOUSEMOTION:
-            checkMotion(event.pos)
-pygame.quit()
-        
-        
+    # Charger une image
+    background = pygame.image.load('ressources/bkgrd.jpg')
+    background.convert()
+
+    # Télécharger les boutons d'images
+    startImg = pygame.image.load('ressources/start.png').convert_alpha()
+    exitImg = pygame.image.load('ressources/exit.png').convert_alpha()
+    logoGitHub = pygame.image.load('ressources/github.png').convert_alpha()
+    npIcone = pygame.image.load('ressources/np.png').convert_alpha()
+    startImgClicked = pygame.image.load('ressources/startClicked.png').convert_alpha()
+    exitImgClicked = pygame.image.load('ressources/exitClicked.png').convert_alpha()
+    logoGitHubClicked = pygame.image.load('ressources/githubClicked.png').convert_alpha()
+
+    listButton=[]
+
+    # Création de boutton
+    startButton = Button(WINDOW_LONG/3,WINDOW_LARG/3,startImg,startImgClicked)
+    exitButton = Button(WINDOW_LONG-exitImg.get_width(),10, exitImg,exitImgClicked)
+    githuButton = Button(25,WINDOW_LARG-logoGitHub.get_height(),logoGitHub,logoGitHubClicked)
+    npButton = Button(WINDOW_LONG-npIcone.get_width(),WINDOW_LARG-npIcone.get_height(),npIcone,npIcone)
+
+    listButton.append(startButton)
+    listButton.append(exitButton)
+    listButton.append(githuButton)
+    listButton.append(npButton)
+
+    # Fonction qui permet de savoir si un elt est cliquer
+    def checkClique(pos):
+        global running
+        if(startButton.rect.collidepoint(pos)):
+            print("start")
+        elif(exitButton.rect.collidepoint(pos)):
+            running=False
+        elif(githuButton.rect.collidepoint(pos)):
+            webbrowser.open("https://github.com/Emjoh60/L3_SAE_4_Jeu_de_Dame")
+
+    # Affichage de la fenêtre
+    running = True
+    while running:
+        screen.blit(background,(0, 0))
+        for bouton in listButton:
+            bouton.redraw()
+        pygame.display.flip()
+            
+        # Boucle pour défénir une action faite par l'utilisateur
+        for event in pygame.event.get(): 
+            # Si personne fait une action on demande de quitter le jeu
+            if event.type == pygame.QUIT: 
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                checkClique(listButton,event.pos)
+            if event.type == pygame.MOUSEMOTION:
+                checkMotion(listButton,event.pos)
+    pygame.quit()
